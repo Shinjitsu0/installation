5673a28df5265866063c141df60109bcec521aad
Vladimir Durov <Shinjitsu0@yandex.ru>
2021-05-18 15:40:56 +0300

    add methods

--- Main.java
+++ Main.java
@@ -3,78 +3,48 @@ package installation;
 import java.io.*;
 
 public class Main {
+  private static StringBuilder log = new StringBuilder();
 
-  public static void main(String[] args) {
-    StringBuilder log = new StringBuilder();
-
-    if (new File("D://Games").mkdir()) {
-      log.append("Каталог Games создан\n");
+  static void makeDir(String path){
+    if (new File(path).mkdir()) {
+      log.append("Каталог " + path + " создан\n");
+    } else{
+      log.append("Ошибка создания " + path + "\n");
     }
+  }
 
-    if (new File("D://Games/src").mkdir()) {
-      log.append("Каталог src создан\n");
-    }
-
-    if (new File("D://Games/res").mkdir()) {
-      log.append("Каталог res создан\n");
-    }
-
-    if (new File("D://Games/savegames").mkdir()) {
-      log.append("Каталог savegames создан\n");
-    }
-
-    if (new File("D://Games/temp").mkdir()) {
-      log.append("Каталог temp создан\n");
-    }
-
-    if (new File("D://Games/src/main").mkdir()) {
-      log.append("Каталог main создан\n");
-    }
-
-    if (new File("D://Games/src/test").mkdir()) {
-      log.append("Каталог test создан\n");
-    }
-
+  static void makeFile(String path){
     try {
-      if (new File("D://Games/src/main/Main.java").createNewFile()) {
-        log.append("Файл Main.java создан\n");
+      if (new File(path).createNewFile()) {
+        log.append("Файл " + path + " создан\n");
       }
     } catch (IOException ex) {
       log.append(ex.getMessage() + "\n");
     }
+  }
 
-    try {
-      if (new File("D://Games/src/main/Utils.java").createNewFile()) {
-        log.append("Файл Utils.java создан\n");
-      }
-    } catch (IOException ex) {
-      log.append(ex.getMessage() + "\n");
-    }
-
-    if (new File("D://Games/res/drawables").mkdir()) {
-      log.append("Каталог drawables создан\n");
-    }
-
-    if (new File("D://Games/res/vectors").mkdir()) {
-      log.append("Каталог vectors создан\n");
-    }
-
-    if (new File("D://Games/res/icons").mkdir()) {
-      log.append("Каталог icons создан\n");
-    }
-
-    try {
-      if (new File("D://Games/temp/temp.txt").createNewFile()) {
-        log.append("Файл temp.txt создан\n");
-      }
-    } catch (IOException ex) {
-      log.append(ex.getMessage() + "\n");
-    }
-
-    try (BufferedWriter writer = new BufferedWriter(new FileWriter("D://Games/temp/temp.txt"))) {
+  static void fileWriter(String path){
+    try (BufferedWriter writer = new BufferedWriter(new FileWriter(path))) {
       writer.append(log);
     } catch (IOException ex) {
       log.append(ex.getMessage() + "\n");
     }
+  }
+
+  public static void main(String[] args) {
+    makeDir("D://Games");
+    makeDir("D://Games/src");
+    makeDir("D://Games/res");
+    makeDir("D://Games/savegames");
+    makeDir("D://Games/temp");
+    makeDir("D://Games/src/main");
+    makeDir("D://Games/src/test");
+    makeFile("D://Games/src/main/Main.java");
+    makeFile("D://Games/src/main/Utils.java");
+    makeDir("D://Games/res/drawables");
+    makeDir("D://Games/res/vectors");
+    makeDir("D://Games/res/icons");
+    makeFile("D://Games/temp/temp.txt");
+    fileWriter("D://Games/temp/temp.txt");
   }
 }
